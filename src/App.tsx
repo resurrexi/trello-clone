80a28fc8525a01ed57424e5a1defd57d4fcc2fd7
Liquan Yang <thedevhiro@gmail.com>
2021-11-19 16:39:03 +0800

    WIP: set layout and allow item creation

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
 import { AppContainer } from './styles'
+import { AddNewItem } from './AddNewItem'
+import { Column } from './Column'
+import { Card } from './Card'
+
 
 export const App = () => {
   return (
     <AppContainer>
-      Columns will go here
+      <Column text="To Do">
+        <Card text="Generate app scaffold" />
+      </Column>
+      <Column text="In Progress">
+        <Card text="Learn Typescript" />
+      </Column>
+      <Column text="Done">
+        <Card text="Begin to use static typing" />
+      </Column>
+      <AddNewItem toggleButtonText="+ Add another list" onAdd={console.log} />
     </AppContainer>
   )
 }
