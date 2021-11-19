71caae833409449937e953b443bf81693784acc2
Liquan Yang <thedevhiro@gmail.com>
2021-11-19 17:15:23 +0800

    add global state context

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,16 @@
 import { AppContainer } from './styles'
 import { AddNewItem } from './AddNewItem'
 import { Column } from './Column'
-import { Card } from './Card'
-
+import { useAppState } from './state/AppStateContext'
 
 export const App = () => {
+  const { lists } = useAppState()
+
   return (
     <AppContainer>
-      <Column text="To Do">
-        <Card text="Generate app scaffold" />
-      </Column>
-      <Column text="In Progress">
-        <Card text="Learn Typescript" />
-      </Column>
-      <Column text="Done">
-        <Card text="Begin to use static typing" />
-      </Column>
+      {lists.map(list => (
+        <Column text={list.text} key={list.id} id={list.id} />
+      ))}
       <AddNewItem toggleButtonText="+ Add another list" onAdd={console.log} />
     </AppContainer>
   )
--- src/Column.tsx
+++ src/Column.tsx
@@ -1,16 +1,24 @@
-import { FC } from 'react'
 import { ColumnContainer, ColumnTitle } from './styles'
 import { AddNewItem } from './AddNewItem'
+import { Card } from './Card'
+import { useAppState } from './state/AppStateContext'
 
 type ColumnProps = {
   text: string
+  id: string
 }
 
-export const Column: FC<ColumnProps> = ({ text, children }) => {
+export const Column = ({ text, id }: ColumnProps) => {
+  const { getTasksByListId } = useAppState()
+
+  const tasks = getTasksByListId(id)
+
   return (
     <ColumnContainer>
       <ColumnTitle>{text}</ColumnTitle>
-      {children}
+      {tasks.map(task => (
+        <Card text={task.text} key={task.id} id={task.id} />
+      ))}
       <AddNewItem
         toggleButtonText="+ Add another task"
         onAdd={console.log}
