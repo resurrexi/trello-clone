248d44499ab6a0dab503530dccf3b66451943686
Liquan Yang <thedevhiro@gmail.com>
2021-11-19 20:57:36 +0800

    add action/reducer for moving items

--- src/state/actions.ts
+++ src/state/actions.ts
@@ -4,6 +4,12 @@ export type Action = {
 } | {
   type: "ADD_TASK"
   payload: { text: string, listId: string }
+} | {
+  type: "MOVE_LIST"
+  payload: {
+    draggedId: string
+    hoverId: string
+  }
 }
 
 export const addTask = (text: string, listId: string): Action => ({
@@ -17,4 +23,12 @@ export const addTask = (text: string, listId: string): Action => ({
 export const addList = (text: string): Action => ({
   type: "ADD_LIST",
   payload: text
+})
+
+export const moveList = (draggedId: string, hoverId: string): Action => ({
+  type: "MOVE_LIST",
+  payload: {
+    draggedId,
+    hoverId
+  }
 })
--- src/utils/arrayUtils.ts
+++ src/utils/arrayUtils.ts
@@ -2,9 +2,26 @@ type Item = {
   id: string
 }
 
-export const findItemIndexById = <TItem extends Item>(
+export function findItemIndexById<TItem extends Item>(
   items: TItem[],
   id: string
-) => {
+) {
   return items.findIndex((item: TItem) => item.id === id)
+}
+
+export function moveItem<TItem>(array: TItem[], from: number, to: number) {
+  const item = array[from]
+  return insertItemAtIndex(removeItemAtIndex(array, from), item, to)
+}
+
+export function removeItemAtIndex<TItem>(array: TItem[], index: number) {
+  return [...array.slice(0, index), ...array.slice(index + 1)]
+}
+
+export function insertItemAtIndex<TItem>(
+  array: TItem[],
+  item: TItem,
+  index: number
+) {
+  return [...array.slice(0, index), item, ...array.slice(index)]
 }
